ffed9a4092ec5aa33eba3f80c0f5d4f3d70a8936
Jerwin <jdbueson@gmail.com>
2025-09-21 22:05:01 +0800

    1

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 
+
+
+class JournalEntry(models.Model):
+    date = models.DateField()
+    reference = models.CharField(max_length=20)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.reference} - {self.date}"
+    
+
+class GeneralJournal(models.Model):
+    entry = models.ForeignKey(JournalEntry, on_delete=models.PROTECT)
+    accounts = models.ForeignKey('chartsofaccounts.chartsofaccounts', on_delete=models.PROTECT)
+    debit_amount = models.DecimalField(max_digits=30)
+    credit_amount = models.DecimalField(max_digits=30)
+
+    def __str__(self):
+        return f"{self.reference} - {self.accounts}"
